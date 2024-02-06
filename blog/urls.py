f8cb5f7f7601e455fec783bf31e4ad057b23079b
Altaf khan <100771268+ialtafkhan@users.noreply.github.com>
2024-02-06 16:44:56 +0530

    Commit #1

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import home, post, category
+
+
+urlpatterns = [
+   
+    path('', home),
+    path('blog/<slug:url>', post),
+    path('category/<slug:url>',category)
+   
+    
+]
